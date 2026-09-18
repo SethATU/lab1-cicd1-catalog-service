4c6691ffc669fa6fb6b8c984e4af80890b95b6cf
Seth Butler <g00467621@atu.ie>
2026-09-18 10:34:37 +0100

    Added controller with get and post

--- src/main/java/ie/atu/cicd1/catalog/service/ProductService.java
+++ src/main/java/ie/atu/cicd1/catalog/service/ProductService.java
@@ -1,10 +1,12 @@
 package ie.atu.cicd1.catalog.service;
 
 import ie.atu.cicd1.catalog.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ProductService {
     private final List<Product> products = new ArrayList<>();
     private long nextId = 1;
